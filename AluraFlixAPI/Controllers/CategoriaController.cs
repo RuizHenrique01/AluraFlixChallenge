0b003a1a1a77e7b2494c60d2e0ea44d1160314c7
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2022-08-28 10:35:40 -0400

    feat: adding Categoria Controller

---
+++ AluraFlixAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,36 @@
+﻿using AluraFlixAPI.Data.Dtos.Categoria;
+using AluraFlixAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AluraFlixAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriaController : ControllerBase
+    {
+        private CategoriaService _categoriaService;
+
+        public CategoriaController(CategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategoria([FromBody] CreateCategoriaDto createCategoriaDto)
+        {
+            ReadCategoriaDto categoria = _categoriaService.CreateCategoria(createCategoriaDto);
+
+            return CreatedAtAction(nameof(FindOneCategoria), new {Id = categoria.Id}, categoria);
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult FindOneCategoria(int id)
+        {
+            ReadCategoriaDto readCategoriaDto = _categoriaService.FindOneCategoria(id);
+
+            if (readCategoriaDto == null) return NotFound("Categoria não encontrada");
+            return Ok(readCategoriaDto);
+        }
+    }
+}
